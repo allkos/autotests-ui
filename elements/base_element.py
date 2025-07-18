d45e77a1767ef8e03c635d44a05604b79445a57b
allkos <a.s.kosarev@yandex.ru>
2025-07-18 20:30:26 +0300

    Практические применение PageFactory в автотестах. Практикуемся в применении Page Factory

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page,Locator, expect
+from playwright.sync_api import Page, Locator, expect
 
 
 class BaseElement:
@@ -7,21 +7,21 @@ class BaseElement:
         self.name = name
         self.locator = locator
 
-    # Метод принимает кейворд аргументы (kwargs)
-    def get_locator(self, **kwargs) -> Locator: # объект Locator для взаимодействия с элементом
-        # Инициализирует объект локатора, подставляя динамические значения в локатор.
+    def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         locator = self.locator.format(**kwargs)
-        # Возвращаем объект локатора
-        return self.page.get_by_test_id(locator)
+        return self.page.get_by_test_id(locator).nth(nth)
 
-    def click(self, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def click(self, nth: int = 0, **kwargs):
+        # Добавили аргумент nth и передеаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         locator.click()
 
-    def check_visible(self, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_visible(self, nth: int = 0, **kwargs):
+        # Добавили аргумент nth и передеаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_be_visible()
 
-    def check_have_text(self, text: str, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_have_text(self, text: str, nth: int = 0, **kwargs):
+        # Добавили аргумент nth и передеаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_have_text(text)
